9dfaa8b68fd78760b04c54c01e436a02f6932013
Yasiru Ashan <yasiruashan2000@gmail.com>
2025-08-25 21:44:28 +0530

    add error constants for handling application errors

---
+++ src/common/constant/errors.constant.ts
@@ -0,0 +1,19 @@
+export const ERRORS = {
+    BAD_REQUEST : {
+key:'BAD_REQUEST',
+message: 'INVALID syntax or missing parameters'
+    
+},
+UNAUTORIZED : {
+    key: 'UNAUTORIZED',
+    message: 'You are not allowed to access this resource'
+},
+LOGIN_FAILED : {
+    key:'LOGIN_FAILED',
+    message: 'INVALID username or password'
+},
+GET_FAILED:{
+    key: 'GET_FAILD',
+    message: 'Failed to get the resource'
+}
+}
